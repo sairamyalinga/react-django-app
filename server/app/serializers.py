a964904d7a8d422d5346781e564a301ecf86d7a7
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-08-21 10:01:12 -0700

    add apis for recipes

---
+++ server/app/serializers.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from .models import Recipes
+
+class RecipeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Recipes
+        fields = ('title','cuisine','instructions','difficulty','rating','author')
--- server/app/views.py
+++ server/app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import RecipeSerializer
+from .models import Recipes
 
 # Create your views here.
+
+class RecipeView(viewsets.ModelViewSet):
+    serializer_class = RecipeSerializer
+    queryset = Recipes.objects.all()

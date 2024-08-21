c6e73bb271358a7fb4fae5636f82114beb50a050
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-08-20 22:18:16 -0700

    adds db model

--- server/app/admin.py
+++ server/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Recipes
+
+class RecipesAdmin(admin.ModelAdmin):
+    fields = ('title','cuisine','instructions','difficulty','rating','author')
 
 # Register your models here.
+
+admin.site.register(Recipes, RecipesAdmin)
--- server/app/models.py
+++ server/app/models.py
@@ -1,5 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
 
+class Recipes(models.Model):
+    title = models.TextField()
+    cuisine = models.TextField()
+    instructions = models.TextField()
+    difficulty = models.CharField(max_length=120)
+    rating = models.IntegerField()
+    author = models.TextField()
+    
+    def _str_(self):
+        return self.title
 

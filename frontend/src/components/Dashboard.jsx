1f6dd819ff62884b1edf03bc05825fa07f9e2ed1
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-08-22 23:46:48 -0700

    adds dashboard layout

---
+++ frontend/src/components/Dashboard.jsx
@@ -0,0 +1,12 @@
+function Dashboard() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+return (
+    <div>
+        <h1>Recipes</h1>
+    </div>
+);
+}
+
+export default Dashboard;

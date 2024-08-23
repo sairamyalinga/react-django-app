ff6b607c5483eb2f07acf0c3fa0738aa8c0050b3
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-08-22 23:47:27 -0700

    adds axios call

--- frontend/src/components/Dashboard.jsx
+++ frontend/src/components/Dashboard.jsx
@@ -2,6 +2,27 @@ function Dashboard() {
   const [data, setData] = useState([]);
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    axios
+      .get("http://127.0.0.1:8000/api/recipes/")
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      })
+      .catch((error) => {
+        setError(error.message || "Something went wrong");
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+}
+
+if (error) {
+    return <p>Error: {error}</p>;
+}
+
 return (
     <div>
         <h1>Recipes</h1>

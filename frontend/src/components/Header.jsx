fd6a6af4edc5174b1098ef7b1dd229c7388a89e4
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-08-22 23:45:01 -0700

    adds handlecreate function definition

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,17 @@
 import RestaurantMenuFilledIcon from "@mui/icons-material/RestaurantMenuOutlined";
 
+const handleCreateRecipe = () => {
+
+}
 function Header() {
   return (
     <header className="sticky top-0 z-50 flex w-full bg-white shadow-md dark:bg-gray-800">
       <div className="flex flex-grow items-center justify-between px-6 py-4 bg-gradient-to-r from-pink-500 to-orange-300">
         <div className="flex flex-grow items-center px-6 py-4">
           <RestaurantMenuFilledIcon />
-          <div className="text-white font-bold text-lg ml-2">Recipes</div>
+          <div className="text-white font-bold text-lg ml-2" >Recipes</div>
         </div>
-        <button className="bg-white text-pink-500 font-semibold py-2 px-4 rounded-lg hover:bg-gray-100 transition duration-200">
+        <button className="bg-white text-pink-500 font-semibold py-2 px-4 rounded-lg hover:bg-gray-100 transition duration-200" onClick={handleCreateRecipe}>
           Create Recipe
         </button>
       </div>
